7143dadff7464d0067bcea0595aac6d73dcc1fc0
Matthew Capper <matthew.capper01@gmail.com>
2019-05-10 08:26:02 -0500

    5-10 825a

--- acquire_msc.py
+++ acquire_msc.py
@@ -32,7 +32,7 @@ def get_repo_list():
     return repo_urls
 
 
-def get_all_readme_files_and_languages(urls, use_cache=True):
+def get_all_readme_files_and_languages(use_cache=True):
     '''checks for a local cache to use if possible or requested in kwargs and proceeds to hit a list of 
     urls for repositories and collects the primary coding language associated with each, in addition to 
     the body of the readme.md in each repository page as displayed inline by github'''
@@ -41,7 +41,7 @@ def get_all_readme_files_and_languages(urls, use_cache=True):
         return repositories_list
     else:
         BASE_URL = 'https://github.com'
-        SECTIONS = urls
+        SECTIONS = get_repo_list()
         repositories = []
 
         for section in SECTIONS:
@@ -60,10 +60,15 @@ def get_all_readme_files_and_languages(urls, use_cache=True):
                         primary_language = repo.find_all(class_='lang')[1].text
                     else:
                         primary_language = repo.find_all(class_='lang')[0].text
+                    if type(repo.find(class_='markdown-body entry-content p-5')) == None:
+                        readme = 'Readme Not Located'
+                    else:
+                        readme = repo.find(class_='markdown-body entry-content p-5').text.strip()
+
                 this_entry = {
                             'title': section,
                             'language': primary_language,
-                            'content': (repo.find(class_='markdown-body entry-content p-5').text.strip())
+                            'content': readme
                          }
                 print(this_entry.keys())
                 print(this_entry['title'])
@@ -76,37 +81,50 @@ def get_all_readme_files_and_languages(urls, use_cache=True):
 # The below code was done to read data files because github blocked the Codeup server 
 # from making too many requests.  
 
-def temp_get_all_readme_files_and_languages():
-    BASE_URL = 'https://github.com'
-    SECTIONS = ['1','2','3']
-    articles = []
-
-    for section in SECTIONS:   
-        with open("page"+ section + ".html") as file: # Use file to refer to the file object
-            data = file.read()
-
-        soup = bsoup(data, features="lxml")
-        
-        # CREATE A LIST OF DICTIONARIES:
-        # Iterating through all of the relevant articles, extract the title and content
-        # Also add the category to the dictionary.
-
-        for repo in soup.find_all(class_="repository-content"):
-            if len(repo.find_all(class_='lang')) > 1:
-                if repo.find_all(class_='lang')[0] == 'Jupyter NoteBook':
-                    primary_language = repo.find_all(class_='lang')[1].text
-                else:
-                    primary_language = repo.find_all(class_='lang')[0].text
-            this_entry = {
-                        'title': section,
-                        'language': primary_language,
-                        'content': (repo.find(class_='markdown-body entry-content p-5').text.strip())
-                        }
-            print(this_entry.keys())
-            print('this is printing the page number only for now:',this_entry['title'])
-            print('primary language', this_entry['language'])
-            articles.append(this_entry)
-
-    return articles
 
 # Now we have the web pages in a list of dictionaries
+
+def get_alt(repo_list, use_cache = False):
+    '''checks for a local cache to use if possible or requested in kwargs and proceeds to hit a list of 
+    urls for repositories and collects the primary coding language associated with each, in addition to 
+    the body of the readme.md in each repository page as displayed inline by github'''
+    if use_cache and os.path.exists('repositories.json'):
+        repositories_list = json.load(open('repositories.json'))
+        return repositories_list
+    else:
+        BASE_URL = 'https://github.com'
+        SECTIONS = repo_list
+        repositories = []
+
+        for section in SECTIONS:
+            print('github name: ', section)
+            url = f'{BASE_URL}/{section}'
+            print('url:', url)
+            response = requests.get(url)
+            soup = bsoup(response.text, features="lxml")
+        
+         # CREATE A LIST OF DICTIONARIES:
+         # Iterating through all of the relevant repositories, extract the title and content
+         # Also add the category to the dictionary.
+            for repo in soup.find_all(class_="repository-content"):
+                if len(repo.find_all(class_='lang')) > 1:
+                    if repo.find_all(class_='lang')[0] == 'Jupyter NoteBook':
+                        primary_language = repo.find_all(class_='lang')[1].text
+                    else:
+                        primary_language = repo.find_all(class_='lang')[0].text
+                    if repo.find(class_='markdown-body entry-content p-5').text:
+                        readme = repo.find(class_='markdown-body entry-content p-5').text
+                    else:
+                        readme = 'Readme not located'
+
+                this_entry = {
+                            'title': section,
+                            'language': primary_language,
+                            'content': readme
+                         }
+                print(this_entry.keys())
+                print(this_entry['title'])
+                print(this_entry['language'])
+                repositories.append(this_entry)
+        json.dump(repositories, open('repositories.json', 'w'))
+        return repositories
